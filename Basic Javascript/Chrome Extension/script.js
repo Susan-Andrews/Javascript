8966c00404be275b11c95222e9dcfb7c6d8f218e
Susan-Andrews <susan@annsusanandrews.com>
2023-02-23 23:52:28 +0530

    editeed

--- Basic Javascript/Chrome Extension/script.js
+++ Basic Javascript/Chrome Extension/script.js
@@ -1,27 +1,28 @@
 
 
 
-let myleads=["www.demo.com" ,"www.example.com" ,"www.bin.com"]
+let myleads=[] 
 const inputEl=document.getElementById("input-el")
 const inputbtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
 
 inputbtn.addEventListener("click" , function(){
    myleads.push(inputEl.value)
-   console.log(myleads)
+   renderleads()
 })
+function renderleads(){
+    let listItems=""
+    for (let i=0;i<myleads.length;i++){
+        listItems += "<li>" + myleads[i] + "</li>"
+        ulEl.innerHTML=listItems
+        // ulEl.innerHTML += "<li>" + myleads[i] + "</li>"
+        // or this can be done by:
+        // create element 
+        // set text content 
+        // append to ul 
 
-for (let i=0;i<myleads.length;i++){
-    ulEl.innerHTML += "<li>" + myleads[i] + "</li>"
-    // or this can be done by:
-    // create element 
-    // set text content 
-    // append to ul 
-
-    // const li=document.getElementById("li")
-    // li.textContent=myleads[i]
-    // ulEl.append(li)
-
-
-
+        // const li=document.getElementById("li")
+        // li.textContent=myleads[i]
+        // ulEl.append(li)
+    }
 }
