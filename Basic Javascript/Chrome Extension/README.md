5716c596610f97a565b0f9b4ef76f215f89b2b55
Susan-Andrews <susan@annsusanandrews.com>
2023-02-25 23:06:43 +0530

    localstorage

---
+++ Basic Javascript/Chrome Extension/README.md
@@ -0,0 +1,7 @@
+# How to deploy?  
+
+- We are adding a manifest.json file,used to store and send data.Here we use it to configure our data and to provide metadata about it.  
+- Download all the files and zip the files in any directory. 
+- Open up the developers mode in chrome extensions.Open up load unpacked and trace to the location where you download the files.  
+- Leads tracker extension will be included along with the extensions and hence can be used to store website links or extensions  
+
--- Basic Javascript/Chrome Extension/script.js
+++ Basic Javascript/Chrome Extension/script.js
@@ -1,19 +1,32 @@
 
 
 
-let myleads=[] 
+let myleads=[]
+// myleads=JSON.parse(myleads)  // turn the myleads string into an array
+// myleads.push("https://github.com/Susan-Andrews?tab=repositories") //Push value into an array
+// myleads=JSON.stringify(myleads) // turn the array into string again
+// console.log(myleads)
 const inputEl=document.getElementById("input-el")
 const inputbtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
 
-localStorage.setItem("myLead" ,"https://github.com/Susan-Andrews" ) //setting item to local storage
-console.log(localStorage.getItem("myLead" )) //retrieving the info
-localStorage.clear() //clearing the infos
+// localStorage.setItem("myLead" ,"https://github.com/Susan-Andrews123" ) //setting item to local storage
+// console.log(localStorage.getItem("myLead" )) //retrieving the info
+// localStorage.clear() //clearing the infos
+let leadsfromlocalstorage=JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsfromlocalstorage)
+if(leadsfromlocalstorage){
+    myleads=leadsfromlocalstorage
+    renderleads()
+}
+
 
 inputbtn.addEventListener("click" , function(){
    myleads.push(inputEl.value)
    inputEl.value=""
+   localStorage.setItem("myleads",JSON.stringify(myleads))
    renderleads() //when called the whole function happens
+   console.log(myleads)
 })
 function renderleads(){
     let listItems=""
@@ -42,3 +55,4 @@ function renderleads(){
 }
 
 
+//here everything is in strings so inorder to store array in localstorage,we use json.parse() or json.stringify to convert to string 
